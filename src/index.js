7e114ab3d32c9b4b92ef69fa9733b019d3cb8655
Carlos Cortez <carlos.cfcortez@gmail.com>
2019-02-05 12:11:59 -0300

    alter port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ app.get('/', (req, res) => {
     return res.send('hello');
 });
 
+var PORT = process.env.PORT || 4000;
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
     console.log('Server started on port 3000');
 });
